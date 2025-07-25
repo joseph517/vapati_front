1f769e2ad906278dbf24c7ccf8203c0dbc8c3dcc
alvaro <alvarovergara1993@hotmail.com>
2025-07-25 02:19:10 -0500

    feat create service to registe user

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,9 +3,12 @@ import { enviroments } from '../../../enviroments/enviroments';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { UserResponse } from '../interfaces/user-response.interface';
+import { CategoryResponse } from '../interfaces/category-response.interface';
+import { UserRequest } from '../interfaces/user-request.interface';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  
   private readonly baseUrl = enviroments.baseUrl;
   constructor(private readonly http: HttpClient) {}
 
@@ -17,7 +20,17 @@ export class AuthService {
           localStorage.setItem('accessToken', response.accessToken);
           localStorage.setItem('refreshToken', response.refreshToken);
           localStorage.setItem('user', JSON.stringify(response.userInfo));
-        })
-      );
+        }
+      )
+    );
   }
+
+  getCategories(): Observable<CategoryResponse[]> {
+    return this.http.get<CategoryResponse[]>(`${this.baseUrl}api/categories/list`);
+  }
+
+  register(user: UserRequest): Observable<UserResponse> {
+    return this.http.post<UserResponse>(`${this.baseUrl}api/users/create`, user);
+  }
+
 }
